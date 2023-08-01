3c6ffb858f9654f8ea4d7dc5a62d203536d917db
T1 <126980610+tadash10@users.noreply.github.com>
2023-08-01 11:38:50 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# API configuration
+API_BASE_URL = "https://api.example.com"
+API_HEADERS = {"Authorization": "Bearer YOUR_API_KEY"}
+
+# Rate limiting settings
+RATE_LIMIT_CAPACITY = 100
+RATE_LIMIT_REFILL_RATE = 10
+MAX_REQUESTS_PER_SECOND = 5
